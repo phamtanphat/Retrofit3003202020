b3905dca1636149458423740b5ed51405149fb78
phatdroid94 <phatdroid94@gmail.com>
2020-06-29 21:31:32 +0700

    retrofit

--- app/src/main/java/com/example/retrofit3003202020/ApiRequest.java
+++ app/src/main/java/com/example/retrofit3003202020/ApiRequest.java
@@ -1,5 +1,6 @@
 package com.example.retrofit3003202020;
 
+import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface ApiRequest {
@@ -7,4 +8,5 @@ public interface ApiRequest {
 //    https://khoapham.vn/KhoaPhamTraining/ (end point)
 //    https://khoapham.vn/KhoaPhamTraining/json/tien/demo1.json
     @GET("json/tien/demo1.json")
+    Call<Demo1> getDataDemo1();
 }
--- app/src/main/java/com/example/retrofit3003202020/MainActivity.java
+++ app/src/main/java/com/example/retrofit3003202020/MainActivity.java
@@ -5,10 +5,21 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Protocol;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.Retrofit.Builder;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -23,17 +34,46 @@ public class MainActivity extends AppCompatActivity {
         //Bước 1 : Khởi tạo retrofit
 
         //Định nghĩa khởi tạo gson
-        Gson gson = new GsonBuilder().setLenient().disableHtmlEscaping().create();
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .disableHtmlEscaping()
+                .create();
+
+        // OkhttpClient
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                                    .readTimeout(30, TimeUnit.SECONDS)
+                                    .writeTimeout(30,TimeUnit.SECONDS)
+                                    .connectTimeout(30,TimeUnit.SECONDS)
+                                    .retryOnConnectionFailure(true)
+                                    .protocols(Arrays.asList(Protocol.HTTP_1_1))
+                                    .build();
+
+
         Retrofit retrofit = new Retrofit.Builder()
-                            .baseUrl("https://khoapham.vn/KhoaPhamTraining/")
+                            .baseUrl("http://khoapham.vn/KhoaPhamTraining/")
                             .addConverterFactory(GsonConverterFactory.create(gson))
+                            .client(okHttpClient)
                             .build();
 
         //Bước 2 : Khơi tạo interface request
         ApiRequest apiRequest = retrofit.create(ApiRequest.class);
 
         //Bước 3 : Gọi hàm request
+        Call<Demo1> apiDataDemo1 = apiRequest.getDataDemo1();
 
+        apiDataDemo1.enqueue(new Callback<Demo1>() {
+            @Override
+            public void onResponse(Call<Demo1> call, Response<Demo1> response) {
+                Demo1 demo1 = response.body();
+                Log.d("BBB",demo1.getLogo());
+            }
+
+            @Override
+            public void onFailure(Call<Demo1> call, Throwable t) {
+                Log.d("BBB",t.getMessage());
+            }
+        });
 
     }
 }
